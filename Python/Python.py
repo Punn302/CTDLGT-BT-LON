010ba541aeb971ac89436abe95d81b3494dbd7ee
ansiro2023 <vinhdq2023@gmail.com>
2024-05-23 17:31:43 +0700

    Nothing special

---
+++ Python/Python.py
@@ -0,0 +1,79 @@
+#Khai bao toan cuc
+
+
+
+
+
+
+
+
+
+#Khoi tao, nhap gia tri
+
+
+
+
+
+
+
+
+
+#Doc phan tu
+
+
+
+
+
+
+
+
+
+#Them phan tu
+
+
+
+
+
+
+
+
+
+#Xoa phan tu
+
+
+
+
+
+
+
+
+
+#Tim kiem thong tin
+
+
+
+
+
+
+
+
+
+#Duyet va sap xep cac phan tu
+
+
+
+
+
+
+
+
+
+#Viet chuong trinh quan ly du lieu sach bang Python
+
+
+
+
+
+
+
+
